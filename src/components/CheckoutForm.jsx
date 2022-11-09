51b04c72fec2f65cf0a0bed0f8f6f23bb9aced4e
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-11-09 23:04:09 +0100

    thank you message

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,35 +1,45 @@
 import { insertOrder } from "../modules/db";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function CheckoutForm(props) {
   const theForm = useRef(null);
+  const [paymentCompleted, setPaymentCompleted] = useState(false);
 
-  function submit(e) {
+  async function submit(e) {
     e.preventDefault();
-
-    insertOrder({
+    const response = await insertOrder({
       name: theForm.current.elements.name.value,
       email: theForm.current.elements.email.value,
       address: theForm.current.elements.address.value,
       basket: props.cart,
     });
+    console.log(response);
+    if (response && response.length) {
+      setPaymentCompleted(true);
+    }
   }
   return (
-    <form onSubmit={submit} ref={theForm}>
-      <div className="form-control">
-        <label htmlFor="form-name">Name</label>
-        <input required type="text" name="name" id="form-name" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-email">Email</label>
-        <input required type="email" name="email" id="form-email" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-address">Address</label>
-        <textarea required name="address" id="form-address" />
-      </div>
-      <button>Pay</button>
-    </form>
+    <>
+      {paymentCompleted ? (
+        <p>THANK YOU!</p>
+      ) : (
+        <form onSubmit={submit} ref={theForm}>
+          <div className="form-control">
+            <label htmlFor="form-name">Name</label>
+            <input required type="text" name="name" id="form-name" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-email">Email</label>
+            <input required type="email" name="email" id="form-email" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-address">Address</label>
+            <textarea required name="address" id="form-address" />
+          </div>
+          <button>Pay</button>
+        </form>
+      )}
+    </>
   );
 }
 
--- src/modules/db.js
+++ src/modules/db.js
@@ -1,16 +1,16 @@
-export function insertOrder(payload) {
+export async function insertOrder(payload) {
   const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpzanpvcm1hcmF3ZXJjZ3Bkc3RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgwMTExNTAsImV4cCI6MTk4MzU4NzE1MH0.AQ3CKjlHItzWkJeqmLJzYwPWttrIHgNgnn25W7_FaWA";
   const url = "https://zsjzormarawercgpdstq.supabase.co";
 
-  fetch(url + "/rest/v1/simpleshop", {
+  const res = await fetch(url + "/rest/v1/simpleshop", {
     method: "POST",
+    // "Content-Type": "application/JSON",
     headers: {
-      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpiZWt2dmRhY2JwcXBhbWNpd2tnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg2OTksImV4cCI6MTk4MjE3NDY5OX0.-tQpiAuUMYTHKDXLgXIo5WUVhC3lOodaVpOCXrKQoe0",
-      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpiZWt2dmRhY2JwcXBhbWNpd2tnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg2OTksImV4cCI6MTk4MjE3NDY5OX0.-tQpiAuUMYTHKDXLgXIo5WUVhC3lOodaVpOCXrKQoe0",
+      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpzanpvcm1hcmF3ZXJjZ3Bkc3RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgwMTExNTAsImV4cCI6MTk4MzU4NzE1MH0.AQ3CKjlHItzWkJeqmLJzYwPWttrIHgNgnn25W7_FaWA",
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpzanpvcm1hcmF3ZXJjZ3Bkc3RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgwMTExNTAsImV4cCI6MTk4MzU4NzE1MH0.AQ3CKjlHItzWkJeqmLJzYwPWttrIHgNgnn25W7_FaWA",
       Prefer: "return=representation",
     },
     body: JSON.stringify(payload),
   });
-  //   .then()
-  //   .then()
+  return await res.json();
 }
